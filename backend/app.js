d3bb087a19d09887063383c08ae11e0816140bc1
Yassine Saadaoui <saadaouiy51@gmail.com>
2024-10-20 13:17:11 +0100

    env BackEnd

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+// to use the env file 
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
